3de2afdff2a08d8bafac24e5fd6a626d5305b025
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-03-31 15:03:33 +1300

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,8 @@
+<?php
+echo "apple","," ,"grapes",",","banana";
+$a=print (" This line is printed using print <br>Hi there");
+print ($a);
+$b=2;
+print "apple " . "grapes" ;
+print "the value of the a is " . $a . "<br>"  ;
+?>
